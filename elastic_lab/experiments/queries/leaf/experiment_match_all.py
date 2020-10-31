33adb30e982835db52780447a62a144b4b4aa7d3
IgooorGP <igor.feq@gmail.com>
2020-10-31 20:53:29 -0300

    add new search query experiments

--- elastic_lab/experiments/queries/leaf/experiment_match_all.py
+++ elastic_lab/experiments/queries/leaf/experiment_match_all.py
@@ -1,17 +1,82 @@
-from uuid import uuid4
-
 from elastic_lab.tools.infra.elasticsearch import elastic_client
 from elastic_lab.tools.services.seed import seed_elasticsearch_with_pipe_products
+from elastic_lab.tools.services.seed import temporary_index
 
 
-def experiment_match_all_should_query_all_documents():
-    # arrange
-    index_name = f"pipes-index-1-{uuid4()}"
-    seed_elasticsearch_with_pipe_products(index_name)
+def experiment_should_get_all_documents_using_match_all_query():
+    with temporary_index(elastic_client) as index_name:
+        # arrange
+        seed_elasticsearch_with_pipe_products(index_name)
 
-    # act
-    query = {"query": {"match_all": {}}}
-    es_response = elastic_client.search(index=index_name, body=query)
+        # act
+        query = {"query": {"match_all": {}}}
+        es_response = elastic_client.search(index=index_name, body=query)
 
-    # assert
-    assert es_response["hits"]["total"]["value"] == 6
+        # assert
+        assert es_response["hits"]["total"]["value"] == 6
+
+
+def experiment_should_get_no_docs_using_term_query():
+    with temporary_index(elastic_client) as index_name:
+        # arrange
+        seed_elasticsearch_with_pipe_products(index_name)
+
+        # act: should not find as 'description' field is text and has been analyzed (tokenized)
+        # so the qry shouldn't find any docs with term (which does not analyze the searched term)
+        # fmt: off
+        query = {
+            "query": {
+                "term": {
+                    "description": "PVC water pipe elbow joint of 90 degrees"
+                }
+            }
+        }
+        # fmt: on
+        es_response = elastic_client.search(index=index_name, body=query)
+
+        # assert
+        assert es_response["hits"]["total"]["value"] == 0
+
+
+def experiment_should_get_all_docs_using_match_query():
+    with temporary_index(elastic_client) as index_name:
+        # arrange
+        seed_elasticsearch_with_pipe_products(index_name)
+
+        # act: should find all docs as the tokenized terms like 'PVC', 'water', 'pipe' will mostly
+        # like match all docs
+        # fmt: off
+        query = {
+            "query": {
+                "match": {
+                    "description": "PVC water pipe elbow joint of 90 degrees"
+                }
+            }
+        }
+        # fmt: on
+        es_response = elastic_client.search(index=index_name, body=query)
+
+        # assert
+        assert es_response["hits"]["total"]["value"] == 6
+
+
+def experiment_should_get_two_docs_using_match_phrase_query():
+    with temporary_index(elastic_client) as index_name:
+        # arrange
+        seed_elasticsearch_with_pipe_products(index_name)
+
+        # act: should find all docs as the tokenized terms like 'PVC', 'water', 'pipe' will mostly
+        # like match all docs
+        # fmt: off
+        query = {
+            "query": {
+                "match_phrase": {
+                    "description": "PVC water pipe elbow joint of 90 degrees"
+                }
+            }
+        }
+        # fmt: on
+        es_response = elastic_client.search(index=index_name, body=query)
+
+        # assert
+        assert es_response["hits"]["total"]["value"] == 2
--- elastic_lab/tools/services/seed.py
+++ elastic_lab/tools/services/seed.py
@@ -1,6 +1,11 @@
 """
 Service for seeding data on Elasticsearch to be used for the tests.
 """
+from contextlib import contextmanager
+from typing import Optional
+from uuid import uuid4
+
+from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 
 from elastic_lab.settings import ELASTICSEARCH_SEEDS
@@ -26,3 +31,18 @@ def seed_elasticsearch_with_pipe_products(index_name: str):
     pipe_products_json_path = ELASTICSEARCH_SEEDS["pipe_products"]
 
     seed_elasticsearch_with_json(index_name, pipe_products_json_path)
+
+
+@contextmanager
+def temporary_index(client: Elasticsearch, temp_index_name: Optional[str] = None):
+    """
+    Temporarily creates a test index name which is deleted at the end of the context manager.
+    """
+    temp_index_name = temp_index_name if temp_index_name is not None else f"test-index-{uuid4()}"
+
+    # creates and yields the test index name
+    client.indices.create(temp_index_name)
+    yield temp_index_name
+
+    # deletes the temp index after the test
+    client.indices.delete(temp_index_name)
