3e9b12c0c5fe8fb4f6b0f7ef4079385b6dd43cd9
IgooorGP <igor.feq@gmail.com>
2020-11-01 11:46:46 -0300

    add match_phrase and match_phrase_prefix study

--- elastic_lab/experiments/queries/leaf/README.md
+++ elastic_lab/experiments/queries/leaf/README.md
@@ -7,3 +7,26 @@ Unlike `match` queries, the `term` queries **do not analyze** the search term be
 The `term` query should be used **exact matches** including whitespace and capitalization. This type of query can be useful for fields like `name.keyword` due to the fact that `keyword` fields are saved **without any text analysis** (stored as is).
 
 Hence, for full text searching, the `match` queries are more well suited due to the tokenization and token filtering that are made to the searched field.
+
+## match_phrase VS match_phrase_prefix
+
+These two `match` queries will analyze (tokenize) the searched terms. However, `match_phrase` will require the search tokens in the same
+order and EXACTLY as they appear whereas the `match_phrase_prefix` the LAST TOKEN will be used as PREFIX and not an exact match:
+
+```text
+{
+    "query": {
+        "match_phrase": {
+            "title": "The Legend Of Z" --> ["the", "legend", "of", "z"]  (analyzer: last token is an EXACT match)
+        }
+    }
+}
+
+{
+    "query": {
+        "match_phrase_prefix": {
+            "title": "The Legend Of Z" --> ["the", "legend", "of", "z.*"]  (analyzer: last token is a prefix!)
+        }
+    }
+}
+```
--- elastic_lab/experiments/queries/leaf/experiment_leaf_queries.py
+++ elastic_lab/experiments/queries/leaf/experiment_leaf_queries.py
@@ -1,3 +1,5 @@
+from elasticsearch.helpers import bulk
+
 from elastic_lab.tools.infra.elasticsearch import elastic_client
 from elastic_lab.tools.services.seed import seed_elasticsearch_with_pipe_products
 from elastic_lab.tools.services.seed import temporary_index
@@ -82,3 +84,46 @@ def experiment_should_get_two_docs_using_match_phrase_query():
 
         # assert
         assert es_response["hits"]["total"]["value"] == 2
+
+
+def experiment_should_check_differences_between_match_phrase_and_match_phrase_prefix_queries():
+    with temporary_index(elastic_client) as index_name:
+        # arrange
+        actions = [
+            {"_index": index_name, "_source": {"title": "The Legend Of Zelda: Majora's Mask"}},
+            {"_index": index_name, "_source": {"title": "The Legend Of Zelda: Ocarina Of Time"}},
+            {"_index": index_name, "_source": {"title": "The Legend Of Zelda: Breath Of The Wild"}},
+            {"_index": index_name, "_source": {"title": "The Legend Of Zelda: A Link To The Past"}},
+            {"_index": index_name, "_source": {"title": "The Legend Of Z: Something Else"}},  # no prefix matches this!
+        ]
+        bulk(elastic_client, actions)
+        elastic_client.indices.refresh(index_name)
+
+        # fmt: off
+        query_match_phrase_no_prefix = {
+            "query": {
+                "match_phrase": {
+                    "title": "The Legend Of Z"  # search terms must match in the same order and as they are
+                }
+            }
+        }
+
+        query_match_phrase_prefix = {
+            "query": {
+                "match_phrase_prefix": {
+                    "title": "The Legend Of Z"  # search terms must match in the same order but the last one is a PREFIX
+                }
+            }
+        }
+        # fmt: on
+
+        # act
+        es_response_no_prefix = elastic_client.search(index=index_name, body=query_match_phrase_no_prefix)
+        es_response_prefix = elastic_client.search(index=index_name, body=query_match_phrase_prefix)
+
+        # assert -> with match_phrase, the last term requires an EXACT match!
+        assert es_response_no_prefix["hits"]["total"]["value"] == 1
+        assert es_response_no_prefix["hits"]["hits"][0]["_source"]["title"] == "The Legend Of Z: Something Else"
+
+        # assert -> with match_phrase_prefix, the last term is a PREFIX and does not require an exact match!
+        assert es_response_prefix["hits"]["total"]["value"] == 5
